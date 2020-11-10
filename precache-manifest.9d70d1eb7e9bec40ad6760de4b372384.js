66add8f94cd855e9e4e093d741c4a1e85864bc92
hanhuayun <2256988633@qq.com>
2020-11-10 17:14:41 +0800

    ok

---
+++ precache-manifest.9d70d1eb7e9bec40ad6760de4b372384.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fadcc3338d8f53e4e53f676ed0e54753",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1357adb64ce18d9b32f9",
+    "url": "/static/css/2.f98a1de1.chunk.css"
+  },
+  {
+    "revision": "b34a985626c3b4f1cb9e",
+    "url": "/static/css/main.a7f633e6.chunk.css"
+  },
+  {
+    "revision": "1357adb64ce18d9b32f9",
+    "url": "/static/js/2.683275d2.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/static/js/2.683275d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b34a985626c3b4f1cb9e",
+    "url": "/static/js/main.242a29b9.chunk.js"
+  },
+  {
+    "revision": "e2702865a21868f2ebe6",
+    "url": "/static/js/runtime-main.7918a0be.js"
+  }
+]);
